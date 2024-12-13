3569cef18a2c992ed8b0d497988529887bc06d1c
Allen Schmerler <allenschmerler@gmail.com>
2024-12-12 19:45:22 -0500

    2015/6 done.

--- 2015/day-5/python/solution.py
+++ 2015/day-5/python/solution.py
@@ -1,13 +1,30 @@
 import re
-input = open("2015/day-5/python/sampleInput.txt").readlines()
+input = open("2015/day-5/python/input.txt").readlines()
 
 def checkVowels(str):
-    return len(set(re.findall("[aeiou]", str))) >= 3
-    
+    return len((re.findall("[aeiou]", str))) >= 3
 def checkDouble(str):
-    pass
+    for c in range(len(str)-1):
+        if str[c] == str[c+1]:
+            return True
+    return False
 def checkBlacklist(str):
-    pass
+    for b in ["ab", "cd", "pq", "xy"]:
+        if str.find(b) != -1:
+            return False
+    return True
+def checkRepeatingPair(str):
+    for i in range(len(str)-2):
+        for j in range(len(str)-2):
+            if str[i:i+2] == str[j:j+2]:
+                if abs(i-j) > 1:
+                    return True
+    return False
+def checkRepeatingLetterWithSpace(str):
+    for i in range(len(str)-2):
+        if str[i] == str[i+2]:
+            return True
+    return False
 
-for i in input:
-    print(i.strip(), checkVowels(i))
+print([checkVowels(i) and checkDouble(i) and checkBlacklist(i) for i in input].count(True))
+print([checkRepeatingPair(i) and checkRepeatingLetterWithSpace(i) for i in input].count(True))
